51879bef8fac7a36c504a4ea2f779278ed1d9eef
Aaron Maslen <aaron.maslen@gmail.com>
2024-04-19 12:58:15 +1000

    Better KeyboardAccess patch Silence annoying Owlcat EventSystem message

---
+++ LinqExtensions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MicroUtils.Linq
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<(int index, T item)> Indexed<T>(this IEnumerable<T> source)
+        {
+            var index = 0;
+            foreach (var item in source)
+                yield return (index++, item);
+        }
+
+        public static IEnumerable<T> FindSequence<T>(this IEnumerable<T> source, int length, IEnumerable<Func<T, bool>> predicateSequence)
+        {
+            var i = 0;
+            foreach (var result in predicateSequence.Zip(source, (f, x) => f(x)))
+            {
+                if (!result) return source.Skip(1).FindSequence(length, predicateSequence);
+
+                i++;
+
+                if (i >= length) return source.Take(i);
+            }
+
+            return Enumerable.Empty<T>();
+        }
+
+        public static IEnumerable<T> FindSequence<T>(this IEnumerable<T> source, IEnumerable<Func<T, bool>> predicateSequence) =>
+            source.FindSequence(predicateSequence.Count(), predicateSequence);
+
+        public static IEnumerable<T> FindSequence<T>(this IEnumerable<T> source, int length, Func<IEnumerable<T>, bool> predicate)
+        {
+            var subSeq = source.Take(length);
+            if (subSeq.Count() < length) return Enumerable.Empty<T>();
+
+            if (predicate(subSeq)) return subSeq;
+
+            return source.Skip(1).FindSequence(length, predicate);
+        }
+
+        public static (T, IEnumerable<T>) Pop<T>(this IEnumerable<T> source) => (source.First(), source.Skip(1));
+
+        /// <summary>
+        /// Selects distinct elements from a sequence, first applying a selector function and using a provided equality comparer
+        /// </summary>
+        public static IEnumerable<T> DistinctBy<T, U>(this IEnumerable<T> seq, Func<T, U> selector, IEqualityComparer<U> comparer)
+        {
+            var distinct = new List<U>();
+
+            foreach (var item in seq)
+            {
+                var selected = selector(item);
+
+                if (!distinct.Contains(selected, comparer))
+                {
+                    distinct.Add(selected);
+                    yield return item;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates a dictionary from a sequence of Key/Value pairs
+        /// </summary>
+
+        /// <typeparam name="TKey">Key type</typeparam>
+        /// <typeparam name="TValue">Value type</typeparam>
+        /// <param name="source">Source sequence</param>
+        public static IDictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<(TKey key, TValue value)> source) =>
+            source.ToDictionary(kv => kv.key, kv => kv.value);
+
+        /// <summary>
+        /// Creates a dictionary from a sequence of Key/Value pairs using a provided <see cref="IEqualityComparer{T}"/>
+        /// </summary>
+        /// <param name="source">Source sequence</param>
+        /// <param name="keyComparer">Key equality comparer</param>
+        public static IDictionary<TKey, TValue> ToDictionary<TKey, TValue>
+            (this IEnumerable<(TKey key, TValue value)> source, IEqualityComparer<TKey> keyComparer) =>
+            source.ToDictionary(kv => kv.key, kv => kv.value, keyComparer);
+
+
+        /// <summary>
+        /// Selects distinct elements from a sequence, first applying a selector function and using the default equality comparer
+        /// </summary>
+        public static IEnumerable<T> DistinctBy<T, U>(this IEnumerable<T> seq, Func<T, U> selector) => DistinctBy(seq, selector, EqualityComparer<U>.Default);
+    }
+}
--- Main.cs
+++ Main.cs
@@ -1,12 +1,25 @@
 ﻿using HarmonyLib;
 
 using Kingmaker;
+using Kingmaker.UI.InputSystems;
+using Kingmaker.UI.Selection;
 
+using MicroUtils.Transpiler;
+
+using Owlcat.Runtime.Core.Logging;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 using UnityExplorer;
@@ -16,83 +29,105 @@ using UnityModManagerNet;
 
 namespace UnityExplorerLoader
 {
+    //    [HarmonyPatch]
+    //    class InputFocusHandler : MonoBehaviour
+    //    {
+    //        public InputField component;
+    //        bool keyboardWasDisabled;
+
+    //        void Update()
+    //        {
+    //            if (component.isFocused)
+    //            {
+    //                if (!Game.Instance.Keyboard.Disabled)
+    //                {
+    ////#if DEBUG
+    ////                    Main.Logger.Log("Disabling keyboard");
+    ////#endif
+    //                    keyboardWasDisabled = true;
+    //                    Game.Instance.Keyboard.Disabled.SetValue(true);
+    //                }
+    //                return;
+    //            }
+
+    //            if (Game.Instance.Keyboard.Disabled && !component.isFocused && keyboardWasDisabled)
+    //            {
+    ////#if DEBUG
+    ////                Main.Logger.Log("Enabling keyboard");
+    ////#endif
+    //                keyboardWasDisabled = false;
+    //                Game.Instance.Keyboard.Disabled.SetValue(false);
+    //            }
+    //        }
+
+    //        [HarmonyPatch(typeof(UIFactory), nameof(UIFactory.CreateInputField))]
+    //        [HarmonyPostfix]
+    //        static InputFieldRef UIFactory_CreateInputField_Postfix(InputFieldRef __return)
+    //        {
+    //            __return.Component.gameObject.AddComponent<InputFocusHandler>().component = __return.Component;
+
+    //            return __return;
+    //        }
+    //    }
+
+    [HarmonyPatch(typeof(KeyboardAccess))]
+    static class KeyboardAccessPatch
+    {
+        [HarmonyPatch(nameof(KeyboardAccess.IsInputFieldSelected)), HarmonyPrefix]
+        static bool Prefix(ref bool __result)
+        {
+            try
+            {
+                EventSystem current = EventSystem.current;
+                if (current == null)
+                    return false;
+                GameObject selectedGameObject = current.currentSelectedGameObject;
+                __result = selectedGameObject != null && selectedGameObject.GetComponent<InputField>() != null;
+                return !__result;
+            }
+            finally
+            {
+            }
+        }
+    }
+
+    [HarmonyPatch]
+    class UnityExplorerLoader : MonoBehaviour
+    {
+        [HarmonyPatch(typeof(GameMainMenu), "Awake")]
+        [HarmonyPostfix]
+        public static void MainMenu_Awake(GameMainMenu __instance)
+        {
+            ExplorerStandalone.CreateInstance(delegate (string msg, LogType logType)
+            {
+                switch (logType)
+                {
+                    case LogType.Error:
+                        Main.Logger.Error(msg);
+                        break;
+                    case LogType.Assert:
+                        Main.Logger.Critical(msg);
+                        break;
+                    case LogType.Warning:
+                        Main.Logger.Warning(msg);
+                        break;
+                    case LogType.Log:
+                        Main.Logger.Log(msg);
+                        break;
+                    case LogType.Exception:
+                        Main.Logger.Error(msg);
+                        break;
+                }
+            });
+        }
+    }
+
     static class Main
     {
-        [HarmonyPatch]
-        class InputFocusHandler : MonoBehaviour
-        {
-            public InputField component;
-            //bool keyboardWasDisabled;
-
-            void Update()
-            {
-                if (component.isFocused)
-                {
-                    if (!Game.Instance.Keyboard.Disabled)
-                    {
-#if DEBUG
-                        Main.Logger.Log("Disabling keyboard");
-#endif
-                        //keyboardWasDisabled = true;
-                        Game.Instance.Keyboard.Disabled.SetValue(true);
-                    }
-                    return;
-                }
-
-                if (/*keyboardWasDisabled && */!component.isFocused)
-                {
-#if DEBUG
-                    Main.Logger.Log("Enabling keyboard");
-#endif
-                    //keyboardWasDisabled = false;
-                    Game.Instance.Keyboard.Disabled.SetValue(false);
-                }
-            }
-
-            [HarmonyPatch(typeof(UIFactory), nameof(UIFactory.CreateInputField))]
-            [HarmonyPostfix]
-            static InputFieldRef UIFactory_CreateInputField_Postfix(InputFieldRef __return)
-            {
-                __return.Component.gameObject.AddComponent<InputFocusHandler>().component = __return.Component;
-
-                return __return;
-            }
-        }
-
-        [HarmonyPatch]
-        static class Loader
-        {
-            [HarmonyPatch(typeof(GameMainMenu), "Awake")]
-            [HarmonyPostfix]
-            public static void MainMenu_Awake()
-            {
-                Main.Logger.Log("Instantiating UnityExplorer");
-                ExplorerStandalone.CreateInstance(delegate (string msg, LogType logType)
-                {
-                    switch (logType)
-                    {
-                        case LogType.Error:
-                            Main.Logger.Error(msg);
-                            break;
-                        case LogType.Assert:
-                            Main.Logger.Critical(msg);
-                            break;
-                        case LogType.Warning:
-                            Main.Logger.Warning(msg);
-                            break;
-                        case LogType.Log:
-                            Main.Logger.Log(msg);
-                            break;
-                        case LogType.Exception:
-                            Main.Logger.Error(msg);
-                            break;
-                    }
-                });
-            }
-        }
-
         internal static Harmony HarmonyInstance;
         internal static UnityModManager.ModEntry.ModLogger Logger;
+
+        internal static ExplorerStandalone UnitExplorer;
 
         static bool Load(UnityModManager.ModEntry modEntry)
         {
@@ -100,8 +135,12 @@ namespace UnityExplorerLoader
             modEntry.OnUnload = OnUnload;
             modEntry.OnGUI = OnGUI;
             HarmonyInstance = new Harmony(modEntry.Info.Id);
+            
+            var uePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "UnityExplorer.STANDALONE.Mono.dll");
+            
+            Logger.Log($"Loading Unity Explorer from {uePath}");
 
-            Assembly.LoadFrom(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "UnityExplorer", "UnityExplorer.STANDALONE.Mono.dll"));
+            Assembly.LoadFrom(uePath);
 
             HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
             return true;
@@ -114,5 +153,47 @@ namespace UnityExplorerLoader
             HarmonyInstance.UnpatchAll(modEntry.Info.Id);
             return true;
         }
+    }
+}
+
+[HarmonyPatch]
+static class AnnoyingMessagesPatch
+{
+    static MethodBase TargetMethod() =>
+    typeof(KingmakerInputModule)
+        .GetNestedTypes(AccessTools.all)
+        .Where(t => t.GetCustomAttributes<CompilerGeneratedAttribute>().Any())
+        .Select(t => t.GetMethod("MoveNext", AccessTools.all))
+        .FirstOrDefault();
+
+    [HarmonyTranspiler]
+    static IEnumerable<CodeInstruction> CheckEventSystem_Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var matchPattern = new Func<CodeInstruction, bool>[]
+        {
+            ci => ci.opcode == OpCodes.Ldsfld,
+            ci => ci.opcode == OpCodes.Ldstr,
+            ci => ci.Calls(AccessTools.Method(typeof(LogChannel), nameof(LogChannel.Log), [typeof(string)]))
+        };
+
+        bool replaceWithNop(IEnumerable<CodeInstruction> instructions)
+        {
+            var match = instructions.FindInstructionsIndexed(matchPattern);
+
+            if (match.Count() != 3)
+                return false;
+
+            foreach (var (_, instruction) in match)
+            {
+                instruction.opcode = OpCodes.Nop;
+                instruction.operand = null;
+            }
+
+            return true;
+        }
+
+        while (replaceWithNop(instructions)) { }
+
+        return instructions;
     }
 }
